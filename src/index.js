1d3e178510d1b9183f5284fae96b38f8402cd039
VeraEnders <veraabramo@gmail.com>
2023-05-25 00:23:46 +0200

    add animation

--- src/index.js
+++ src/index.js
@@ -8,17 +8,28 @@ function handleSubmit(e) {
   // Get the selected rating using FormData
   const formData = new FormData(e.target);
   const rating = formData.get('rating');
-  
+
+  if (rating) {
+    showNextDisplay(rating);
+  }
+}
+
+function showNextDisplay(rating) {
   // Create a new element from the template content
   const newContent = document.importNode(template.content, true);
 
   // Replace the placeholder with the selected rating
   const ratingTemp = newContent.querySelector('[data-rating]');
   ratingTemp.textContent = rating;
-  
+
   // Replace the existing content with the new content
   wrapper.innerHTML = '';
   wrapper.appendChild(newContent);
+
+  // Trigger the animation by adding the active class
+  setTimeout(() => {
+    document.querySelector('[data-template]').classList.add('active');
+  }, 10);
 }
 
 form.addEventListener('submit', handleSubmit);
